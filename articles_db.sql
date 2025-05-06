006a482c2afeb32725d077d47eb9206e67ab5db0
Bshara karkaby <bshara.karkaby@gmail.com>
2025-05-06 23:06:36 +0300

    Update articles_db.sql

--- articles_db.sql
+++ articles_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2025 at 09:47 PM
+-- Generation Time: May 06, 2025 at 10:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,140 +18,61 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `user_db`
+-- Database: `articles_db`
 --
-CREATE DATABASE IF NOT EXISTS `user_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `user_db`;
+CREATE DATABASE IF NOT EXISTS `articles_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `articles_db`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `articles`
 --
 
-CREATE TABLE `orders` (
+CREATE TABLE `articles` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_items`
---
-
-CREATE TABLE `order_items` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `price` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
+  `author` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `articles`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'John Doe', 'john@example.com', 'hashed_password1', '2025-04-15 21:42:43', '2025-04-15 21:42:43'),
-(2, 'Jane Doe', 'jane@example.com', 'hashed_password2', '2025-04-15 21:42:43', '2025-04-15 21:42:43');
+INSERT INTO `articles` (`id`, `title`, `content`, `author`, `created_at`, `updated_at`) VALUES
+(1, 'Template article 1', 'Text of article 1', 'Author 1', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(2, 'Template article 2', 'Text of article 2', 'Author 2', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(3, 'Template article 3', 'Text of article 3', 'Author 3', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(4, 'Template article 4', 'Text of article 4', 'Author 4', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(5, 'Template article 5', 'Text of article 5', 'Author 5', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(6, 'Template article 6', 'Text of article 6', 'Author 6', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(7, 'Template article 7', 'Text of article 7', 'Author 7', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(8, 'Template article 8', 'Text of article 8', 'Author 8', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(9, 'Template article 9', 'Text of article 9', 'Author 9', '2025-05-06 19:45:42', '2025-05-06 19:45:42'),
+(10, 'Template article 10', 'Text of article 10', 'Author 10', '2025-05-06 19:45:42', '2025-05-06 19:45:42');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `orders`
+-- Indexes for table `articles`
 --
-ALTER TABLE `orders`
+ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_items`
---
-ALTER TABLE `order_items`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `order_id` (`order_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `articles`
 --
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order_items`
---
-ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `order_items`
---
-ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
